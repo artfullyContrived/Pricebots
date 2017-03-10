8c034ab6e174f31e3e27ecab6d87e2b5d850da97
Jordan <jordan.dwo@gmail.com>
2017-03-10 07:36:32 -0700

    Made while loop infinite

--- priceBot.py
+++ priceBot.py
@@ -9,10 +9,10 @@ import tweepy, time, sys
 #argfile = str(sys.argv[1])
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'zQNh4UD3dTfdg6rchdE5WDwXE'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'vnNCSocmdKBkVvEmExQ6Bi3OlszIXoQxXNgtK2xeuv786cHITQ'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '840062077305724928-3aqfuyj6BuLhFslpkokrFXQjW3rU2dh'#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'NNFzYUSwA6iWBaujUibUzRxgPcQEzzflUo5FaomYS8bE2'#keep the quotes, replace this with your access token secret
+CONSUMER_KEY = 'your key...'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = 'your secret...'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = 'your key...'#keep the quotes, replace this with your access token
+ACCESS_SECRET = 'your secret...'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
@@ -24,7 +24,7 @@ api = tweepy.API(auth)
 count = 0
 
 #goes through iteration 9 times
-while (count < 9) :
+while (1) :
     #grabs conents from cryptowatch
     contents = \
         urllib2.urlopen("https://api.cryptowat.ch/markets/coinbase/ethusd/summary")\
@@ -58,7 +58,6 @@ while (count < 9) :
     print datetime.datetime.utcnow()
     api.update_status(tweet)
     #bot sleeps for 10 sec
-    time.sleep(60)
-    count = count + 1
+    time.sleep(3600)
 
 print "All done!"
