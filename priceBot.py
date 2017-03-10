c937d56ce18cc9216f995b24d69959a89b95cbd5
Jordan <jordan.dwo@gmail.com>
2017-03-10 08:20:30 -0700

    Implemented a threading timer
    used a threading timer instead of a while loop

--- priceBot.py
+++ priceBot.py
@@ -1,7 +1,7 @@
 #twitterbot
 #jordan.dwo@gmail.com
 import urllib2
-import time, datetime
+import time, datetime, threading
 import json
 from pprint import pprint
 import tweepy, time, sys
@@ -17,21 +17,15 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-
-#for line in f:
-#    api.update_status(line)
-#    time.sleep(900)#Tweet every 15 minutes
-count = 0
-
-#goes through iteration 9 times
-while (1) :
+def updateTweet ():
+    threading.Timer(60,updateTweet).start()
     #grabs conents from cryptowatch
     contents = \
         urllib2.urlopen("https://api.cryptowat.ch/markets/coinbase/ethusd/summary")\
         .read()
     data = json.loads(contents)
 
-    #finds data
+    #finds data in contents
     last = data['result']['price']['last']
     high = data['result']['price']['high']
     low = data['result']['price']['low']
@@ -50,14 +44,17 @@ while (1) :
     absolute_change = "Change: $%3.2f\n" % absolute_change
     volume = "Volume: $%9.2f\n" % volume
 
+    #creates string for tweet
     tweet = "Ethereum price over last 24hrs:\n" + last + high + low + percentage \
         + absolute_change + volume + "$eth #Ethereum"
 
     #prints data to console
     print "Last tweet sent:"
     print datetime.datetime.utcnow()
-    api.update_status(tweet)
-    #bot sleeps for 10 sec
-    time.sleep(3600)
 
-print "All done!"
+    #tweets to witter
+    api.update_status(tweet)
+
+updateTweet ()
+print "Just tweeted:\n" +str(tweet)
+print
